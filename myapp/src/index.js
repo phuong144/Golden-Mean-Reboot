47a18807662d1ac3ae981f474351173616dad284
Patrick <patrickphuong@ymail.com>
2019-06-17 22:19:42 -0700

    Test

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Banner} from './components/Banner';
 import {Navtest} from './components/Navtest';
-import {LoggedIn} from './components/LoggedIn';
 import {LoggedOut} from './components/LoggedOut';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
