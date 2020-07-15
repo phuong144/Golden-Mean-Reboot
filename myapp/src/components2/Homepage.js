9877488e76ee694f51f832dd88a3ae9cc015ff4a
phuong144 <patrickphuong07@gmail.com>
2020-07-14 17:34:25 -0700

    Clear Logo

--- myapp/src/components2/Homepage.js
+++ myapp/src/components2/Homepage.js
@@ -129,7 +129,7 @@ function Intro(props){
             <p id="I1">Welcome to the <br></br>Golden Mean Podcast <br></br>hosted by Timothy and Swan!</p>
             <p id="I2"><br></br>These podcasts will feature wide-ranging conversations about sports, politics, philosophy, and life in general. We discuss challenging topics but the goal is always to have fun doing it while sharing our perspectives!</p>
         </div>
-        <img id="hlogo" src={require("./images/Logo.jpg")}/>
+        <img id="hlogo" src={require("./images/ClearLogo.png")}/>
         </div>
     )
 }
